7d4780da671519613645ed2458a21169b521f013
William Shackelford <williamshackelford@Williams-MacBook-Pro.local>
2017-06-23 12:53:07 -0400

    v1.0.36

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,13 @@
 (function (global, factory) {
     if (typeof define === "function" && define.amd) {
-        define(['module', './collection', './model', 'babel-polyfill'], factory);
+        define(['module', './collection', './model'], factory);
     } else if (typeof exports !== "undefined") {
-        factory(module, require('./collection'), require('./model'), require('babel-polyfill'));
+        factory(module, require('./collection'), require('./model'));
     } else {
         var mod = {
             exports: {}
         };
-        factory(mod, global.collection, global.model, global.babelPolyfill);
+        factory(mod, global.collection, global.model);
         global.index = mod.exports;
     }
 })(this, function (module, _collection, _model) {
