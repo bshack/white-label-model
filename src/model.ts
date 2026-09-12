b9845a3ce0b8edaae97b69388592ee3d31e69be2
William Shackelford <bshackelford@gmail.com>
2026-09-12 19:45:28 -0400

    refactor: keep event typing out of model runtime source

--- src/model.ts
+++ src/model.ts
@@ -13,19 +13,6 @@ interface ModelMutation<T extends ModelData> {
     state: T;
 }
 
-interface ModelEvents<T extends ModelData> {
-    change: [state: T];
-    mutate: [mutation: ModelMutation<T>];
-    set: [state: T];
-    update: [state: T];
-    push: [state: T];
-    delete: [state: T];
-    clear: [state: T];
-}
-
-type ModelEventName<T extends ModelData> = keyof ModelEvents<T>;
-type ModelEventArguments<T extends ModelData, Name extends ModelEventName<T>> = ModelEvents<T>[Name];
-
 /** Mutable object, array, or Map state with synchronous change notifications. */
 class Model<T extends ModelData = Record<string, unknown>> extends Utilities {
     modelData: T;
@@ -481,34 +468,6 @@ class Model<T extends ModelData = Record<string, unknown>> extends Utilities {
     servicePatch() { return Promise.resolve({}); }
     servicePost() { return Promise.resolve({}); }
     servicePut() { return Promise.resolve({}); }
-}
-
-/** Compile-time event contracts without adding runtime EventEmitter wrappers. */
-interface Model<T extends ModelData = Record<string, unknown>> {
-    on<Name extends ModelEventName<T>>(
-        eventName: Name,
-        listener: (...arguments_: ModelEventArguments<T, Name>) => void
-    ): this;
-    once<Name extends ModelEventName<T>>(
-        eventName: Name,
-        listener: (...arguments_: ModelEventArguments<T, Name>) => void
-    ): this;
-    addListener<Name extends ModelEventName<T>>(
-        eventName: Name,
-        listener: (...arguments_: ModelEventArguments<T, Name>) => void
-    ): this;
-    off<Name extends ModelEventName<T>>(
-        eventName: Name,
-        listener: (...arguments_: ModelEventArguments<T, Name>) => void
-    ): this;
-    removeListener<Name extends ModelEventName<T>>(
-        eventName: Name,
-        listener: (...arguments_: ModelEventArguments<T, Name>) => void
-    ): this;
-    emit<Name extends ModelEventName<T>>(
-        eventName: Name,
-        ...arguments_: ModelEventArguments<T, Name>
-    ): boolean;
 }
 
 export = Model;
