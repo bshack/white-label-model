0b7ebc068414b4f8833fc29c7b7171338858f68b
William Shackelford <bshackelford@gmail.com>
2026-09-12 19:47:53 -0400

    build: refresh generated index runtime

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Model = void 0;
 /** Unified observable state constructor for objects, arrays, and Maps. */
-const Model = require("./model");
+const ModelImplementation = require("./model");
+const Model = ModelImplementation;
 exports.Model = Model;
 //# sourceMappingURL=index.js.map
